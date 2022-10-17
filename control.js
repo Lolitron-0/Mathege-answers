673f0b9dcdab06651ae1c3e303e5f00dcf39d679
Lolitron <Ovcharov_05@mail.ru>
2022-10-17 20:26:03 +0300

    workflow done

--- control.js
+++ control.js
@@ -1,14 +1,18 @@
 function updateCurrentTask() {
-	for(let element of document.tasks) {
-        element.style.backgroundColor = "white"
-    }
+	for (let i = 0; i < document.tasks.length; i++) {
+		let element = document.tasks[i];
+		element.style.backgroundColor = "white";
+	}
 	document.tasks[document.currentTask].style.backgroundColor = "#adffb2";
-	document.tasks[Math.max(document.currentTask-1),0].scrollIntoView(true);
+	document.tasks[Math.max(document.currentTask - 1, 0)].scrollIntoView({
+        block: "center",
+		behavior: "smooth",
+	});
 }
 
 document.currentTask = 0;
 window.onkeydown = (e) => {
-	if (e.key == " ") {
+	if (e.key == "Enter") {
 		document.currentTask += 1;
 		updateCurrentTask();
 	}
